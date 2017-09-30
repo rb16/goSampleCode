d63d81be838486178e78ccfe16a5b2c852c34c34
RB <rb.rana1993@gmail.com>
2017-09-30 11:56:23 +0530

    adding duplicate 2

--- chap1/dup2.go
+++ chap1/dup2.go
@@ -8,15 +8,33 @@ import (
 	"os"
 )
 
-
 func main() {
 	counts := make(map[string]int)
 	files := os.Args[1:]
-	if (len(files) == 0) {
-		countlines(os.Stdin, counts)
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, file := range files {
+			f, err := os.Open(file)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		fmt.Printf("%d\t%s\n", n, line)
 	}
 }
 
-func countlines(f *os.File, counts map[string]int) {
+// countLines read files and scans file by line and splits into words
+// and saves into maps with their frequency
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
 
 }

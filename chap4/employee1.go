3608e11a72e713424cf8f65aa83c9693aea3dace
RB <rb.rana1993@gmail.com>
2017-10-19 17:26:19 +0530

    TODO- POC on non-pointer struct can't identify variableg

--- chap4/employee1.go
+++ chap4/employee1.go
@@ -12,10 +12,46 @@ type Employee struct {
 	DoB       time.Time
 	Position  string
 	Salary    int
-	MAnagerID int
+	ManagerID int
 }
 
+func EmployeeIDD(id int) Employee {
+
+	return Employee{
+		ID:        id,
+		Name:      "my name",
+		Address:   "my address",
+		DoB:       time.Now(),
+		Position:  "manager",
+		Salary:    11111,
+		ManagerID: 11,
+	}
+}
+func EmployeeID(id int) *Employee {
+	return &Employee{
+		ID:        id,
+		Name:      "my name",
+		Address:   "my address",
+		DoB:       time.Now(),
+		Position:  "manager",
+		Salary:    11111,
+		ManagerID: 11,
+	}
+}
 func main() {
-	e := Employee{}
-	fmt.Println(e)
+
+	//e := EmployeeID(11)
+
+	id := EmployeeIDD(11).ID
+	// the assignment statement would not compile since the left hand side
+	// would not identify a variable :(
+	//TODO - POC
+	EmployeeIDD(id).Name = "my new name"
+
+	//fmt.Println(e)
+
+	//p := EmployeeIDD(12)
+	//fmt.Println(p)
+	fmt.Println(EmployeeIDD(12).ID)
+
 }
